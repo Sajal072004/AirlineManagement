7e8c33395f585bb89a37427e5074ac974224e7a5
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-10-31 23:44:29 +0530

    intalled morgan package

--- API_Gateway/src/index.js
+++ API_Gateway/src/index.js
@@ -4,6 +4,11 @@ const app = express();
 
 const PORT = 3005;
 
+app.get('/home' , (req,res) => {
+  return res.json({message: 'Ok'});
+})
+
 app.listen( PORT, () => {
   console.log(`server started at port ${PORT}`);
 })
+
