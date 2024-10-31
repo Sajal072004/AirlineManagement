56c453affcdb2783ea9e6ea2bbe407ad195c91cc
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-01 01:16:14 +0530

    "Updated rateLimit configuration and added authentication check for /bookingservice endpoint using axios"

--- API_Gateway/src/index.js
+++ API_Gateway/src/index.js
@@ -1,28 +1,42 @@
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const rateLimit = require('express-rate-limit');
+const rateLimit = require("express-rate-limit");
 
-const axios = require('axios');
+const axios = require("axios");
 
 const app = express();
 
 const PORT = 3005;
 
 const limiter = rateLimit({
-  windowMs: 2 * 60 * 1000, 
-  max: 5
-})
-
-
+  windowMs: 2 * 60 * 1000,
+  max: 5,
+});
 
 app.use(morgan("combined"));
 app.use(limiter);
 
-app.use('/bookingservice' , (req,res,next) => {
-  console.log('token :' ,req.headers['x-access-token']);
-  next();
+app.use("/bookingservice", async (req, res, next) => {
+  console.log("token :", req.headers["x-access-token"]);
 
+  try {
+    const response = await axios.get(
+      "http://localhost:3001/api/v1/isauthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
+    console.log(response.data);
+    if(response.data.success) next();
+    else return res.status(401).json({
+      message:'unauthorized'
+    })
+  } catch (error) {
+    return res.status(401).json("unauthorized");
+  }
 });
 
 app.use(
