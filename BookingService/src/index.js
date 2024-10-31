7a4ace15f265ab9121ccd98ff5f77cc98409afca
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-01 00:42:21 +0530

    [feat]:add gitignore file

--- BookingService/src/index.js
+++ BookingService/src/index.js
@@ -12,9 +12,9 @@ const setupAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
 
-  app.get('/api/v1/home' , (req,res)=> {
-    return res.json({message: 'Hitting the booking service'});
-  })
+  // app.get('/api/v1/home' , (req,res)=> {
+  //   return res.json({message: 'Hitting the booking service'});
+  // })
 
   app.use('/api' , apiRoutes);
   app.listen(PORT, () => {
